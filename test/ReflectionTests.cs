ba7bac41525da9927691db96793f9cfdea8ee1f9
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-11-02 14:45:19 +0300

    added reflection tests

---
+++ test/ReflectionTests.cs
@@ -0,0 +1,29 @@
+﻿using MessageBus.Utils;
+using NUnit.Framework;
+using System.Linq;
+using System.Reflection;
+
+namespace MessageBus.UnitTests
+{
+    public class ReflectionTests
+    {
+
+        private readonly ReflectionUtil reflectionUtil = new ReflectionUtil();
+
+        [Test]
+        public void Should_Return_All_Referenced_Assemblies()
+        {
+            var assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+            assemblies.Append(Assembly.GetEntryAssembly().GetName());
+            Assert.AreEqual(assemblies.Length + 1, reflectionUtil.GetAssemblies().ToList().Count);
+        }
+
+        [Test]
+        public void Should_Return_All_Types()
+        {
+            var assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(w => Assembly.Load(w));
+            var types = assemblies.SelectMany(w => w.GetTypes());
+            Assert.AreEqual(types.Count(), reflectionUtil.GetTypes(assemblies).Count());
+        }
+    }
+}
